647e58c4798e03c2de1d105b187fc96338241902
Jason <Jason@VS>
2025-05-01 21:49:20 -0400

    header comment

--- Menu.cpp
+++ Menu.cpp
@@ -1,3 +1,14 @@
+/**
+* Author: Jason Lin
+* Assignment: Dungeon Dungeon
+* Date due: May 2, 2025, 2:00pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
 #include "Menu.h"
 #include "Utility.h"
 
--- Utility.cpp
+++ Utility.cpp
@@ -1,3 +1,14 @@
+/**
+* Author: Jason Lin
+* Assignment: Dungeon Dungeon
+* Date due: May 2, 2025, 2:00pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
 #define LOG(argument) std::cout << argument << '\n'
 #define STB_IMAGE_IMPLEMENTATION
 #define NUMBER_OF_TEXTURES 1
